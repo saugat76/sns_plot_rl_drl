afb4508048b1c7880468bb04d24b75c282c42007
Saugat Tripathi <saugat.tripathi76@gmail.com>
2023-05-02 18:30:33 -0400

    additional results

--- sns_plot.py
+++ sns_plot.py
@@ -45,24 +45,27 @@ df_er = df_er[df_er.Log != "logs\\"]
 df_cu = df_cu[df_er.Log != "logs\\"]
 
 # Smooth the scalar data using a rolling mean
-df_er['Smoothed Reward'] = df_er.groupby('Log')['Episodic Reward'].rolling(window=80, min_periods=1, closed='right').mean().values
-df_cu['Smoothed Users'] = df_cu.groupby('Log')['Connected Users'].rolling(window=80, min_periods=1, closed='right').mean().values
+df_er['Smoothed Reward'] = df_er.groupby('Log')['Episodic Reward'].rolling(window=160, min_periods=1, closed='right').mean().values
+df_cu['Smoothed Users'] = df_cu.groupby('Log')['Connected Users'].rolling(window=160, min_periods=1, closed='right').mean().values
 
-labels = ["Distance Threshold = 1000", "Distance Threshold = 0", "Distance Threshold = 250", "Distance Threshold = 500", "Distance Threshold = 750"]
+# labels = ["Distance Threshold = 1000", "Distance Threshold = 0", "Distance Threshold = 250", "Distance Threshold = 500", "Distance Threshold = 750"]
+# labels = ["5 UAVs", "7 UAVs"]
+# labels = ["Level 1: Implicit Exchange", "Level 2: Reward Exchange", "Level 3: Position Exchange"]
+labels = ["MAQL", "MADQL"]
 logs_labels = pd.unique(df_cu.Log)
 labels_dict = dict(zip(logs_labels, labels))
 
-del labels_dict["madql_uav__lvl2__5__1__1682907964"]
-labels_dict["madql_uav__lvl2__5__1__1682907964"] = "Distance Threshold = 1000"
+# del labels_dict["madql_uav__lvl2__5__1__1682907964"]
+# labels_dict["madql_uav__lvl2__5__1__1682907964"] = "Distance Threshold = 1000"
 
-palette = sns.color_palette('Set1', 5)
+palette = sns.color_palette('Set1', 2)
 # Plot the smoothed scalar data using Seaborn
 sns.set_style('whitegrid')
 ax = sns.lineplot(x='Step', y='Smoothed Reward', hue='Log', hue_order= labels_dict.keys(), data=df_er, legend=True, palette=palette, linewidth=1.5)
 handles, _ = ax.get_legend_handles_labels()
 ax.legend(handles[:5], labels_dict.values())
 ax = sns.lineplot(x='Step', y='Episodic Reward', hue='Log', hue_order= labels_dict.keys(), data=df_er, legend=False, palette=palette, alpha = 0.2)
-# ax.set_title('Convergence Plot')
+ax.set_title('Convergence Plot: MAQL vs MADQL with Position Exchange')
 ax.set_xlabel('Episode', fontsize = 14, family = 'Times New Roman')
 ax.set_ylabel('Episodic Reward', fontsize = 14, family = 'Times New Roman')
 plt.show()
@@ -72,7 +75,7 @@ ax = sns.lineplot(x='Step', y='Smoothed Users', hue='Log', hue_order= labels_dic
 handles, _ = ax.get_legend_handles_labels()
 ax.legend(handles[:5], labels_dict.values())
 ax = sns.lineplot(x='Step', y='Connected Users', hue='Log', hue_order= labels_dict.keys(), data=df_cu, legend=False, palette=palette, alpha = 0.2)
-# ax.set_title('Convergence with Connected Users')
+ax.set_title('Convergence with Connected Users: MAQL vs MADQL with Position Exchange')
 ax.set_xlabel('Episode', fontsize = 14, family = 'Times New Roman')
 ax.set_ylabel('Connected Users', fontsize = 14, family = 'Times New Roman')
 plt.show()
